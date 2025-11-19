52948ac41ab95cdf2174850913fd2091d80b2a52
Wcycycy <131581260+Wcycycy@users.noreply.github.com>
2025-11-19 08:16:21 +0000

    update navbar brand name & theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,10 +83,10 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'Dubbing AI',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
