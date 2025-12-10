4a605a1a7d70832d0e43624b0585d3470c936671
Wcycycy <131581260+Wcycycy@users.noreply.github.com>
2025-12-10 09:52:01 +0000

    add seo description and title

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,25 +6,36 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
+import Head from '@docusaurus/Head';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Start Now→
-          </Link>
-        </div>
-      </div>
-    </header>
+    <>        
+        <Head>
+          <meta name="description" content={siteConfig.tagline} />
+          <title>{siteConfig.title}</title>
+          
+          {/* 3. （可选）修复 Robots Tag 缺失 */}
+          <meta name="robots" content="index, follow" />
+        </Head>
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <Heading as="h1" className="hero_title">
+                    {siteConfig.title}
+                </Heading>
+                <p className="hero_subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro">
+                        Start Now
+                    </Link>
+                </div>
+            </div>
+        </header>
+    </>
   );
 }
 
